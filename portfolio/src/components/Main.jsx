5eec208ce555ef9af79fd7018b15e7cd725af952
Pedro Medeiros <pedropwks@gmail.com>
2023-05-21 16:25:21 -0300

    Adiciona section profile

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Profile from './Profile';
 import Skills from './Skills';
 
 class Main extends Component {
   render() {
     return (
       <main>
+        <section>
+          <Profile />
+        </section>
         <section className="main-skills">
           <Skills />
         </section>
