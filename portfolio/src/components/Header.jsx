0f0463eeecf569c056e08dcd389958676009eb60
Pedro Medeiros <pedropwks@gmail.com>
2023-05-22 17:16:44 -0300

    Mudança no header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,30 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import code from '../images/icons-source-code.png';
-import pac from '../images/pac-header.svg';
+import pac from '../images/pac-header.png';
 import coding from '../images/icons-coding.png';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="container-header">
-        <div className="container-name-center">
-          <h1 className="title-main-header">PEDRODEV</h1>
-          <img src={ code } alt="Ícone de código" />
-        </div>
-        <div className="container-icons">
-          <img className="pac" src={ pac } alt="Animação do pacman" />
-          <img className="coding" src={ coding } alt="Ícone laptop com código" />
-        </div>
-        <div className="container-titles">
-          <h1 className="titles-header">SOBRE MIM</h1>
-          <hr />
-          <h1 className="titles-header">CONHECIMENTOS</h1>
-          <hr />
-          <h1 className="titles-header">PROJETOS</h1>
-        </div>
-      </header>
-    )
-  };
-}
+function Header() {
+  const [handleScroll, setHandleScroll] = useState(0)
+
+  useEffect(() => {
+    const bottomBorder = () => {
+      setHandleScroll(window.scrollY)
+
+      if (handleScroll > 50) {
+        document.querySelector("header").classList.add("header-transform")
+      } else {
+        document.querySelector("header").classList.remove("header-transform")
+      }
+    };
+
+    window.addEventListener('scroll', bottomBorder);
+  }, [handleScroll]);
+
+
+  return (
+    <header className="container-header">
+      <div className="container-name-center">
+        <h1 className="title-main-header">PEDRO</h1>
+        <h1 className="title-main-header-dev">DEV</h1>
+        <img src={ code } alt="Ícone de código" />
+      </div>
+      <div className="container-icons">
+        <img className="pac" src={ pac } alt="Animação do pacman" />
+        <img className="coding" src={ coding } alt="Ícone laptop com código" />
+      </div>
+      <div className="container-titles">
+        <h2 className="titles-header">INÍCIO</h2>
+        <h2 className="titles-header">SOBRE</h2>
+        <h2 className="titles-header">CONHECIMENTOS</h2>
+        <h2 className="titles-header">PROJETOS</h2>
+      </div>
+    </header>
+  )
+};
 
 export default Header;
