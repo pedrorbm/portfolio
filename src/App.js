918b98c3432fed037abb2445439ff0bede2e6e40
Pedro Medeiros <pedropwks@gmail.com>
2023-05-30 22:59:16 -0300

    Correção rota

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Portfolio />
+      <Switch>
+        <Route exact path="/" component={ Portfolio } />
+      </Switch>
     )
   };
 }
